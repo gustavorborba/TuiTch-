e6ecd830d2cfdb0be586f0928d57f7c54e52fe5f
gustavorborba <gustavorobaski@hotmail.com>
2015-12-02 21:22:23 -0200

    feat -> refactoring

--- TuiTche/TuiTche.Repositorio.EF/BancoDeDados.cs
+++ TuiTche/TuiTche.Repositorio.EF/BancoDeDados.cs
@@ -27,21 +27,4 @@ namespace TwiTche.Repositorio.EF
             base.OnModelCreating(modelBuilder);
         }
     }
-
-    class UsuarioMap : EntityTypeConfiguration<Usuario>
-    {
-        public UsuarioMap()
-        {
-            ToTable("Usuario");
-
-            HasKey(u => u.Id);
-
-            Property(u => u.NomeCompleto).IsRequired().HasMaxLength(128).HasColumnName("Nome_Completo");
-            Property(u => u.Username).IsRequired().HasMaxLength(50).HasColumnName("Username");
-            Property(u => u.Senha).IsRequired().HasMaxLength(64).HasColumnName("Senha");
-            Property(u => u.Email).IsRequired().HasMaxLength(128).HasColumnName("Email");
-            Property(u => u.Foto).IsOptional().HasMaxLength(128).HasColumnName("Foto");
-            Property(u => u.SexoUsuario).IsRequired().HasColumnName("IdSexoUsuario");
-        }
-    }
 }
---
+++ TuiTche/TuiTche.Repositorio.EF/Mapping/UsuarioMap.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwiTche.Repositorio.EF.Mapping
+{
+    class UsuarioMap : EntityTypeConfiguration<Usuario>
+    {
+        public UsuarioMap()
+        {
+            ToTable("Usuario");
+
+            HasKey(u => u.Id);
+
+            Property(u => u.NomeCompleto).IsRequired().HasMaxLength(128).HasColumnName("Nome_Completo");
+            Property(u => u.Username).IsRequired().HasMaxLength(50).HasColumnName("Username");
+            Property(u => u.Senha).IsRequired().HasMaxLength(64).HasColumnName("Senha");
+            Property(u => u.Email).IsRequired().HasMaxLength(128).HasColumnName("Email");
+            Property(u => u.Foto).IsOptional().HasMaxLength(128).HasColumnName("Foto");
+            Property(u => u.SexoUsuario).IsRequired().HasColumnName("IdSexoUsuario");
+        }
+    }
+}
