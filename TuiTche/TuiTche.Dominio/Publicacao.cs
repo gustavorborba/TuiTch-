05355b01dae12c8f240ef2dba7826b3caaa83141
gustavorborba <gustavorobaski@hotmail.com>
2015-12-02 21:57:47 -0200

    feat -> Mapping publicação

---
+++ TuiTche/TuiTche.Dominio/Publicacao.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TuiTche.Dominio
+{
+    public class Publicacao : EntidadeBase
+    {
+        public string Descricao { get; private set; }
+        public DateTime DataPublicacao { get; private set; }
+
+        public Publicacao()
+        {
+             
+        }
+        public Publicacao(string descricao, DateTime data)
+        {
+            this.Descricao = descricao;
+            this.DataPublicacao = data;
+        }
+
+    }
+}
