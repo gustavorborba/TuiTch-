cc57919e7d8af5370f7566bbe4ba516ecb9d261d
Júlio César Rodrigues de Oliveira <julio31p@hotmail.com>
2015-12-02 20:07:14 -0200

    feat(tuitche): implmentar mapeamento de usuario

---
+++ TwiTche/TuiTche.Dominio/Usuario.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TuiTche.Dominio
+{
+    public class Usuario : EntidadeBase
+    {
+        public string Username { get; private set; }
+        public string NomeCompleto { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public Sexo SexoUsuario { get; set; }
+        public string Foto { get; set; }
+
+        public enum Sexo
+        {
+            MASCULINO = 1,
+            FEMININO = 2
+        }
+
+        public Usuario()
+        {
+
+        }
+
+        public Usuario(int id)
+        {
+            Id = id;
+        }
+
+    }
+}
---
+++ TwiTche/TwiTche.Repositorio.EF/BancoDeDados.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration;
+using TuiTche.Dominio;
+
+namespace TwiTche.Repositorio.EF
+{
+    class BancoDeDados : DbContext
+    {
+        public BancoDeDados()
+            : base("TUITCHE")
+        {
+
+        }
+
+        public DbSet<Usuario> Usuario { get; set; }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new UsuarioMap());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+    class UsuarioMap : EntityTypeConfiguration<Usuario>
+    {
+        public UsuarioMap()
+        {
+            ToTable("Usuario");
+
+            HasKey(u => u.Id);
+
+            Property(u => u.NomeCompleto).IsRequired().HasMaxLength(128).HasColumnName("Nome_Completo");
+            Property(u => u.Username).IsRequired().HasMaxLength(50).HasColumnName("Username");
+            Property(u => u.Senha).IsRequired().HasMaxLength(64).HasColumnName("Senha");
+            Property(u => u.Email).IsRequired().HasMaxLength(128).HasColumnName("Email");
+            Property(u => u.Foto).IsOptional().HasMaxLength(128).HasColumnName("Foto");
+            Property(u => u.SexoUsuario).IsRequired().HasColumnName("IdSexoUsuario");
+        }
+    }
+}
