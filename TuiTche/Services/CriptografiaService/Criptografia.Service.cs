95bf8ef3b538179f9c30d13513ed47bb77d1d86d
unknown <gustavo.borba@NB-002668.cwinet.local>
2015-12-03 15:32:55 -0200

    Criptografia Service adicionada

---
+++ TuiTche/Services/CriptografiaService/Criptografia.Service.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.CriptografiaService
+{
+    public class CriptografiaService
+    {
+        public string Criptografar(string senha)
+        {
+            senha += "#$TuiTche_$#@Salt_@$Bagu@l";
+            return GerarMd5(senha);
+        }
+        private string GerarMd5(string texto)
+        {
+            System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create();
+
+            byte[] inputBytes = Encoding.ASCII.GetBytes(texto);
+            byte[] hash = md5.ComputeHash(inputBytes);
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("X2"));
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ TuiTche/ServicesTest/CriptografiaTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Services.CriptografiaService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ServicesTest
+{
+    [TestClass]
+    public class CriptografiaTest
+    {
+        [TestMethod]
+        public void gerarSenhaCriptografada()
+        {
+            CriptografiaService service = new CriptografiaService();
+            string hash = service.Criptografar("senha");
+
+            Assert.AreEqual("477F158D66F5A884DC3697E2C97813CE", hash);
+        }
+    }
+}
