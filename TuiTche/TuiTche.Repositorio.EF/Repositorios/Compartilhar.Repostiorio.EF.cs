9612c78cac39d34a3a8b82dcd40fff408d1d4d31
gustavorborba <gustavorobaski@hotmail.com>
2015-12-03 01:58:23 -0200

    Repositorio de compartilhamento adicionado

---
+++ TuiTche/TuiTche.Repositorio.EF/Repositorios/Compartilhar.Repostiorio.EF.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TuiTche.Dominio;
+using TuiTche.Repositorio.EF;
+
+namespace TwiTche.Repositorio.EF.Repositorios
+{
+    public class CompartilharRepositorio
+    {
+        BancoDeDados banco;
+
+        public Publicacao ProcurarPorId(int id)
+        {
+            return banco.Publicacao.Find(id);
+        }
+    }
+}
