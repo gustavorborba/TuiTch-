8766ef8b00ee5558351705d0526afa85bf48f49a
gustavorborba <gustavorobaski@hotmail.com>
2015-12-05 21:23:15 -0200

    Tela e model para Cadastro de Usuários

---
+++ TuiTche/TuiTche.WEB.MVC/Models/UsuarioModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TuiTche.WEB.MVC.Models
+{
+    public class UsuarioModel
+    {
+        [Required(ErrorMessage = "Campo Obrigatório")]
+        [StringLength(128,MinimumLength =5, ErrorMessage ="O Campo deve ter entre 5 e 128 caracteres")]
+        public string NomeCompleto { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigatório")]
+        [StringLength(128, MinimumLength = 5, ErrorMessage = "O Campo deve ter entre 5 e 50 caracteres")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage ="Campo Obrigatorio")]
+        [StringLength(15,MinimumLength =7,ErrorMessage ="O campo deve ter entre 7 e 15 caracteres")]
+        public string Senha { get; set; }
+
+        [Display(Name ="Endereco de Email")]
+        [Required(ErrorMessage = "Campo Obrigatorio")]
+        [EmailAddress(ErrorMessage ="Endereco de Email Inválido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigatorio")]
+        public Dominio.Usuario.Sexo Sexo { get; set; }
+
+        [Display(Name = "Foto de Perfil")]
+        public HttpPostedFileBase FotoPerfil { get; set; }
+    }
+}
