7859aa4d1597609e5919ee4876483a1cc3b38f79
gustavorborba <gustavorobaski@hotmail.com>
2015-12-06 17:12:53 -0200

    Feat -> adicionado script para Infinity Scroll

---
+++ TuiTche/TuiTche.WEB.MVC/Models/ScrollModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TuiTche.WEB.MVC.Models
+{
+    public class ScrollModel
+    {
+        public string HTMLString { get; set; }
+    }
+}
