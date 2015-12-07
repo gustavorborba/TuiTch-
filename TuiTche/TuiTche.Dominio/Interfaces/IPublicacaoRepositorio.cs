3251791546c29815440b26dda8af6cdb96644dcd
gustavorborba <gustavorobaski@hotmail.com>
2015-12-06 22:53:59 -0200

    Feat -> Organização de projeto

---
+++ TuiTche/TuiTche.Dominio/Interfaces/IPublicacaoRepositorio.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TuiTche.Dominio.Interfaces
+{
+    public interface IPublicacaoRepositorio
+    {
+        Publicacao BuscarPorPorId(int id);
+        int Criar(Publicacao publicacao);
+        int PublicacaoTagInsert(Publicacao publicacao);
+        IList<Publicacao> ListarPublicacoesDeSeguidores(int id, int limite);
+        IList<Publicacao> BuscarPublicacoesDeUsuario(int id, int limite);
+    }
+}
---
+++ TuiTche/TuiTche.Dominio/Services/RegrasDeNegocio/CurtirService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TuiTche.Dominio;
+using TuiTche.Dominio.Interfaces;
+
+namespace TuiTche.Dominio.Services
+{
+    public class CurtirService
+    {
+        ICurtirRepositorio curtirRepositorio;
+        IPontuacaoRepositorio pontuacaoRepositorio;
+        public CurtirService(ICurtirRepositorio tri, IPontuacaoRepositorio pontuacao)
+        {
+            this.curtirRepositorio = tri;
+            this.pontuacaoRepositorio = pontuacao;
+        }
+
+        public void CurtirPublicacao(int idPublicacao, int IdPublicacaoUsuario, int idUsuario)
+        {
+            const int PontosPorTri = 3;
+            Pontuacao pontuacao = pontuacaoRepositorio.BuscarPontos(IdPublicacaoUsuario);
+            Curtir curtir = new Curtir();
+            curtir.IDPublicacao = idPublicacao;
+            curtir.IDUsuario = idUsuario;
+            curtirRepositorio.AdicionarCurtir(curtir);
+            pontuacao.PontuacaoTotal += PontosPorTri;
+            pontuacaoRepositorio.SomarPontos(pontuacao);
+        }
+    }
+}
