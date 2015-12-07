e24653267f59412fd2b5b2a3d59d35a1a3ad4235
Júlio César Rodrigues de Oliveira <julio31p@hotmail.com>
2015-12-07 15:59:53 -0200

    Add-Migration

---
+++ TuiTche/TuiTche.Repositorio.EF/Migrations/201512071558566_add-migration fix.cs
@@ -0,0 +1,20 @@
+namespace TwiTche.Repositorio.EF.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addmigrationfix : DbMigration
+    {
+        public override void Up()
+        {
+            RenameTable(name: "dbo.Cometario", newName: "Comentario");
+            AddColumn("dbo.Comentario", "DataComentario", c => c.DateTime(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Comentario", "DataComentario");
+            RenameTable(name: "dbo.Comentario", newName: "Cometario");
+        }
+    }
+}
