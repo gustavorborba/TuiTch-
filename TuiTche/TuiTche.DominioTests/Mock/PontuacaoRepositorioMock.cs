9dc2f397e0cd5d576586ecd20942e0b67d43cba2
gustavorborba <gustavorobaski@hotmail.com>
2015-12-07 00:22:34 -0200

    Adicionado Mocks para CurtirRepositorio e PontuacaoRepositorio

---
+++ TuiTche/TuiTche.DominioTests/Mock/PontuacaoRepositorioMock.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TuiTche.Dominio;
+using TuiTche.Dominio.Interfaces;
+
+namespace TuiTche.DominioTests.Mock
+{
+    public class PontuacaoRepositorioMock : IPontuacaoRepositorio
+    {
+        IList<Pontuacao> listaPontos = new List<Pontuacao>();
+
+        public PontuacaoRepositorioMock()
+        {
+            Pontuacao pontuacao = new Pontuacao(1);
+
+            pontuacao.PontuacaoTotal = 0;
+            listaPontos.Add(pontuacao);
+        }
+
+        public void SomarPontos(Pontuacao pontuacao)
+        {
+            
+        }
+
+        public Pontuacao BuscarPontos(int id)
+        {
+            return listaPontos.Where(p => p.Id == id).FirstOrDefault();
+        }
+        public int NumeroPontuacoesUsuarios()
+        {
+            return listaPontos.Count;
+        }
+
+        public int BuscarPontuacaoPorPosicao(int posicaoUsuario)
+        {
+            return listaPontos.Skip(posicaoUsuario).FirstOrDefault().PontuacaoTotal;
+        }
+    }
+}
