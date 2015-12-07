85e2ef7cbfe40a4634a6777eea336757d9003139
gustavorborba <gustavorobaski@hotmail.com>
2015-12-07 00:22:55 -0200

    Adicionado teste para CurtirService

---
+++ TuiTche/TuiTche.DominioTests/CurtirServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TuiTche.Dominio;
+using TuiTche.Dominio.Services;
+using TuiTche.DominioTests.Mock;
+
+namespace TuiTche.DominioTests
+{
+    [TestClass]
+    public class CurtirServiceTest
+    {
+        [TestMethod]
+        public void CurtirPublicacaoTest()
+        {
+            CurtirService service = new CurtirService(new CurtirRepositorioMock(),new PontuacaoRepositorioMock());
+
+            Pontuacao pontuacao = service.CurtirPublicacao(1, 1, 1);
+
+            Assert.AreEqual(3, pontuacao.PontuacaoTotal);
+        }
+    }
+}
